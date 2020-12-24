8ed5bb1dee3c7f43fc4e70b2e86fd22b39bbc1b5
raresmigea <raresini@yahoo.com>
2020-12-24 19:05:30 +0200

    fixed wrong commit

--- script.js
+++ script.js
@@ -34,21 +34,7 @@ function getInputValue() {
     });
   
     document.getElementById('output').innerHTML = newTextStr;
-  curl --location --request GET 'tfcommerce-api-eks.cloudqa.thermofisher.net/api/store/orders/deferred-payments/search' \
-  --header 'Content-Type: application/json' \
-  --header 'userId: 915918' \
-  --header 'countryCode: kr' \
-  --header 'Authorization: Basic dGZkZWZlcnJlZHBheW1lbnRzOnRoZXJtb2ZpccjE=' \
-  --header 'Cookie: JSESSIONID=D330BF758C8F9E916031D25FC86' \
-  --data-raw '{
-  "searchQueryParams": {
-  "orderType": "tc",
-  "dateFrom": "2019-11-20",
-  "dateTo": "2020-11-25"
-  },
-  "requestOptions": {
-  "sortOrder": "asc",
-  "sortProperty": "orderDate"
+
   }
   
   function getUrlResponse(url) {
@@ -63,4 +49,4 @@ function getInputValue() {
    *
    * http://soundcloud.com/oembed?url=http%3A//soundcloud.com/forss/flickermood&format=js&callback=?
    */
-  }'
+  }' 
