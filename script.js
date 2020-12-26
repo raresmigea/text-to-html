9b45aa6f89b7019a2c37eb89b6fed7e266f02fb7
raresmigea <raresini@yahoo.com>
2020-12-27 00:00:59 +0200

    updated link

--- script.js
+++ script.js
@@ -2,27 +2,27 @@ function getInputValue() {
   const inputVal = document.getElementById('input').value;
   const inputLines = inputVal.split('\n');
   inputLines.forEach((arr, index) => {
-    // rule 2: pre-formatted
+    // Rule 2: pre-formatted
     if (arr.includes('`')) {
       inputLines[index] = arr.replace(/`([^`]+)`?/g, '<pre>$1</pre>');
     }
-    // rule 3: header
+    // Rule 3: header
     if (arr.startsWith('#')) {
       const c = arr.split(' ')[0].match(/#/g || []).length;
       inputLines[index] = `<h${c}>${arr.replace(/#/g, '').trim()}</h${c}>`;
     }
-    // rule 4: bold
+    // Rule 4: bold
     if (arr.includes('!')) {
       inputLines[index] = arr.replace(/\!([^!]+)\!/g, '<b>$1</b>');
     }
-    // rule 5: http requests
+    // Rule 5: http requests
     if (arr.includes('{http')) {
       let url = arr.slice(arr.indexOf('http'), arr.indexOf('}'));
       const returnedJson = getUrlResponse(url);
       inputLines[index] = arr.replace(arr, `{${returnedJson}}`);
     }
   });
-  // rule 1: paragraphs
+  // Rule 1: paragraphs
   newTextStr = '';
   inputLines.forEach(function (paragraph) {
     newTextStr += '<p>' + paragraph + '</p>';
